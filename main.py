cad3c2f9c9f969a331fb0cb58699a7b5cc3486a6
Stephen Thacker <35053174+StephenThacker@users.noreply.github.com>
2025-10-26 21:40:18 -0500

    Update main.py
    Starting to add in Binomial Asset Pricing Model for Call options.

--- main.py
+++ main.py
@@ -5,13 +5,160 @@ from scipy.stats import norm
 import datetime as dt
 import plotly.graph_objects as go
 from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 
+class Node:
+    def __init__(self, node_stock_price):
+        self.node_stock_price = node_stock_price
+        self.value = None
+        self.option_value = None
+        self.up = None  
+        self.down = None
+        self.node_expected_price_value = None
+        self.current_time = 0
+        self.node_layer = 1
+        self.node_number = 1
+        #contains a list of all nodes that are connecting to this node on the upstream side of the graph. 
+        # Each node is represented as a list, which countains a touple and a string
+        #The touple gives the coordinates of the node, the string gives the direction of the branch that the prior node is emitting. ("i.e., up, or down")
+        self.backwards_nodes = []
+
+class binomial_tree:
+
+    def __init__(self, strike_price, final_node_val, number_of_layers, initial_stock_price, initial_sigma, interest_rate, time_to_expiration, stock_dividend):
+        self.strike_price = strike_price
+        print(strike_price)
+        print(type(strike_price))
+        self.final_node_val = final_node_val
+        self.number_of_layers = number_of_layers
+        self.root = Node(initial_stock_price)
+        self.sigma = initial_sigma
+        self.time_to_expiration = time_to_expiration
+        self.interest_rate = interest_rate
+        self.delta_t = None
+        self.dividend = stock_dividend
+        self.node_list = []
+        self.define_time_segment()
+        self.final_option_value = self.create_tree_structure()
+        print(self.final_option_value)
+
+    def calculate_probability(self):
+        try:
+            self.probability = (np.exp((self.interest_rate-self.dividend) * self.delta_t) - self.d) / (self.u - self.d)
+        except ZeroDivisionError:
+            raise ValueError("Division by zero in probability calculation (u == d).")
+        return
+    
+    def define_time_segment(self):
+        self.time_to_expiration = self.time_to_expiration/365
+        try:
+            self.delta_t = self.time_to_expiration / (self.number_of_layers -1)
+        except ZeroDivisionError:
+            raise ValueError
+        
+        self.u = np.exp(self.sigma * np.sqrt(self.delta_t))
+        self.d = np.exp(-self.sigma * np.sqrt(self.delta_t))
+        return self.delta_t
+    
+    def create_tree_nodes(self):
+        for i in range(self.number_of_layers):
+            sub_list_array = [Node(None) for j in range(i + 1)]
+            self.node_list.append(sub_list_array)
+        return
+    
+    def calculate_all_node_stock_values(self):
+        # Set the root layer price from self.root (assumes node_list[0][0] is root level)
+        if self.node_list and self.node_list[0]:
+            self.node_list[0][0].node_stock_price = self.root.node_stock_price
+
+        # For each graph layer
+        for i in range(1, self.number_of_layers):
+            # Iterate across all nodes in layer
+            for k in range(len(self.node_list[i])):
+                current_node = self.node_list[i][k]
+                backwards_nodes_list = current_node.backwards_nodes
+                prices = []  # Collect computed prices from all parents
+                for m, backward in enumerate(backwards_nodes_list):
+                    node_tuple, node_direction = backward
+                    node_tuple_x, node_tuple_y = node_tuple
+                    try:
+                        parent_node = self.node_list[node_tuple_x][node_tuple_y]
+                        if node_direction == "up":
+                            computed_price = parent_node.node_stock_price * self.u
+                        elif node_direction == "down":
+                            computed_price = parent_node.node_stock_price * self.d
+                        else:
+                            continue  # Skip unknown directions
+                        prices.append(computed_price)
+                    except (IndexError, AttributeError) as e:
+                        print(f"Error at layer {i}, node {k}, parent {m}: {e}")
+                        continue
+                
+                # Assign average price (handles recombination; single parent = the price itself)
+                if prices:
+                    current_node.node_stock_price = np.mean(prices)
+                else:
+                    current_node.node_stock_price = 0.0  # Fallback for no parents
+                
+       
+        return
+    
+   
+    def create_tree_structure(self):
+        self.create_tree_nodes()
+        self.create_recombining_tree_branches()
+        self.calculate_all_node_stock_values()
+        self.calculate_probability()
+        return self.determine_option_value()
+
+    
+    def determine_option_value(self):
+        #assigns values of options for the last layer of the tree
+        last_layer_index = self.number_of_layers - 1
+        for node in self.node_list[last_layer_index]:
+            difference = node.node_stock_price - self.strike_price
+            node.option_value = np.maximum(difference,0)
+
+        count = last_layer_index - 1
+        while count >= 0:
+            for node in self.node_list[count]:
+                up_node = node.up.option_value
+                down_node = node.down.option_value
+                node.option_value = self.assign_node_a_value(up_node, down_node)
+                print(node.option_value)
+            count = count - 1
+
+        return self.node_list[0][0]
+        
+    
+    def assign_node_a_value(self, node_value_up, node_value_down):
+        node_value = (np.exp(-1*(self.interest_rate- self.dividend)*self.delta_t)*(self.probability*node_value_up + (1 -self.probability)*node_value_down))
+        return node_value      
+    
+    def create_recombining_tree_branches(self):
+        count = len(self.node_list) -1
+        while 0 <= count -1:
+            for j in range(0,len(self.node_list[count -1])):
+                self.node_list[count-1][j].up = self.node_list[count][j]
+                self.node_list[count -1][j].down = self.node_list[count][j+1]
+                self.node_list[count][j].backwards_nodes.append([(count-1,j),"up"])
+                self.node_list[count][j+1].backwards_nodes.append([(count-1,j),"down"])
+            count = count - 1
+
+        return
+   
+   
+
+    
+    
 
 class options_chain:
     
-    def __init__(self, ticker):
+    def __init__(self, ticker, method):
         self.ticker = ticker
         self.options_chains_dict = {}
+        self.method = method
         self.fetch_options_data(self.ticker)
 
     def vega(self, stock_price, time_to_expiration, cont_div_yield, d_1):
@@ -30,8 +177,8 @@ class options_chain:
         d2 = d_1 - sigma*np.sqrt(time_to_expiration)
         return d2
     
-    def newton_raphson_method(self, epsilon,stock_price, option_price,risk_free_rate, contin_div_yield, time_to_expiration, strike_price):
-        sigma = 0.2
+    def newton_raphson_method_black_scholes(self, epsilon,stock_price, option_price,risk_free_rate, contin_div_yield, time_to_expiration, strike_price):
+        sigma = 0.8
         black_scholes_cost = 1
         market_cost = 0
         max_iter = 400
@@ -51,17 +198,44 @@ class options_chain:
             count += 1
         return sigma
     
-    def dates_to_expiration(self, expiration_date):
+    def dates_to_expiration_fraction(self, expiration_date):
         expiration_date = dt.datetime.strptime(expiration_date, "%Y-%m-%d").date()
         days_to_expiration = (expiration_date - dt.date.today()).days
         fraction_of_days = days_to_expiration/365
         return fraction_of_days
+    
+    def dates_to_expiration_days(self,expiration_date):
+        expiration_date = dt.datetime.strptime(expiration_date, "%Y-%m-%d").date()
+        days_to_expiration = (expiration_date - dt.date.today()).days
+        return days_to_expiration
     
     def num_dates_to_expir(self,expiration_date):
         expiration_date = dt.datetime.strptime(expiration_date, "%Y-%m-%d").date()
         days_to_expiration = (expiration_date - dt.date.today()).days
 
         return days_to_expiration
+    
+    def calc_implied_volatility(self):
+        if self.method == 'Black Scholes':
+            for key in self.options_chains_dict.keys():
+                date_fraction = self.dates_to_expiration_fraction(key)
+                self.options_chains_dict[key]['calcImpliedVol'] = np.vectorize(self.newton_raphson_method_black_scholes)(1e-5, self.last_stock_price,self.options_chains_dict[key]['lastPrice'],self.risk_free,self.dividend_yield,date_fraction,self.options_chains_dict[key]['strike'])
+                self.options_chains_dict[key]['daystoExpir'] = np.vectorize(self.num_dates_to_expir)(key)
+
+        if self.method == "BinTree Continuous Deriv":
+            count = 1
+            for key in self.options_chains_dict.keys():
+                dates_to_expiration = self.dates_to_expiration_days(key)
+                self.options_chains_dict[key]['daystoExpir'] = np.vectorize(self.num_dates_to_expir)(key)
+                if count ==1:
+                    bin_tree = binomial_tree(180,None, 50,self.last_stock_price,0.4,self.risk_free,dates_to_expiration, self.dividend_yield)
+                    count = 0
+
+                else:
+                    continue
+                print("Break")
+
+        return
 
     def fetch_options_data(self, ticker):
         self.ticker = yf.Ticker(ticker)
@@ -70,21 +244,16 @@ class options_chain:
         self.calls = options.calls
         self.info = self.ticker.info
         self.options_chains_dict = {exp : self.ticker.option_chain(exp).calls for exp in expiration_dates}
-        last_stock_price = self.info.get('regularMarketPrice') or ticker.history(period="1d")['Close'][0]
-        risk_free = yf.Ticker("^IRX").history(period="1d")['Close'][-1]/100
+        self.last_stock_price = self.info.get('regularMarketPrice') or ticker.history(period="1d")['Close'][0]
+        self.risk_free = yf.Ticker("^IRX").history(period="1d")['Close'][-1]/100
         if yf.Ticker(ticker).info.get('dividendYield') != None:
-            dividend_yield = yf.Ticker(ticker).info.get('dividendYield')/100
+            self.dividend_yield = yf.Ticker(ticker).info.get('dividendYield')/100
         else:
-            dividend_yield = 0
+            self.dividend_yield = 0
 
-        for key in self.options_chains_dict.keys():
-            date_fraction = self.dates_to_expiration(key)
-            self.options_chains_dict[key]['calcImpliedVol'] = np.vectorize(self.newton_raphson_method)(1e-5, last_stock_price,self.options_chains_dict[key]['lastPrice'],risk_free,dividend_yield,date_fraction,self.options_chains_dict[key]['strike'])
-            self.options_chains_dict[key]['daystoExpir'] = np.vectorize(self.num_dates_to_expir)(key)
-            print(self.options_chains_dict[key].columns)
-            print(self.options_chains_dict[key].head())
-            print(self.options_chains_dict[key]['calcImpliedVol'])
-            print(self.options_chains_dict[key]['daystoExpir'])
+
+        self.calc_implied_volatility()
+
 
         return
     
@@ -102,9 +271,8 @@ class options_chain:
         if last_price is None:
             last_price = yf.Ticker(self.ticker.ticker).history(period="1d")['Close'][0]
 
-        # ±15% of ATM price now
-        lower_strike = last_price * 0.85
-        upper_strike = last_price * 1.15
+        lower_strike = last_price * 0.9
+        upper_strike = last_price * 1.1
 
         max_days_to_exp = 60
 
@@ -121,6 +289,7 @@ class options_chain:
                         (df['calcImpliedVol'] > 0) & \
                         (df['calcImpliedVol'] <= 2.0) & \
                         (df['strike'] >= lower_strike) & (df['strike'] <= upper_strike)
+            
 
             strikes = df.loc[valid_mask, 'strike'].values
             maturity = days_to_exp
@@ -175,13 +344,11 @@ class options_chain:
         fig.show()
 
 
-
-
-
-
 def main():
-    nvda_options = options_chain("TSLA")
-    nvda_options.plot_imp_vol_surface()
+    nvda_options = options_chain("GOOG", "BinTree Continuous Deriv")
+    #nvda_options = options_chain("GOOG", "Black Scholes")
+
+    #nvda_options.plot_imp_vol_surface()
 
 if __name__ == "__main__":
     main()
